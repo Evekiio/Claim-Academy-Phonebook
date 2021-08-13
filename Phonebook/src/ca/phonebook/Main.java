1596b2ae29096725eb652cadd9ff24cc5aa43365
Sanders Riddle <sandersriddle@gmail.com>
2021-08-13 09:02:29 -0600

    Update Main.java

--- Phonebook/src/ca/phonebook/Main.java
+++ Phonebook/src/ca/phonebook/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		// Instantiate Menu Object
+		// Instantiate Menu Object & Controls Application Loop
 		Menu menu = new Menu();
 		
 		// Display Main Menu to User Console
